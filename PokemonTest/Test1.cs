73d8554e8fb8b5b038998c6bf80d42302b15c702
Gregory Guillory <gguillory2015@gmail.com>
2025-10-23 14:09:58 -0500

    Initial commit for unit testing files

---
+++ PokemonTest/Test1.cs
@@ -0,0 +1,20 @@
+﻿namespace PokemonTest;
+
+using Pokemon;
+
+[TestClass]
+public class Test1
+{
+    [TestMethod]
+    public void Test_getPokemonResponse()
+    {
+        string validName = "charizard";
+        Program.getPokemonResponse(validName);
+
+
+
+    }
+}
+
+
+
