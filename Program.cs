861dd5a758260706ed1bde33d61d82216cb3efd5
Gregory Guillory <gguillory2015@gmail.com>
2025-10-23 13:15:28 -0500

    Convert Program.cs to "main" style

--- Program.cs
+++ Program.cs
@@ -9,158 +9,166 @@ using System.Collections.Generic;
 using System.Net;
 
 
-string BASE_POKEMON_ADDRESS = "https://pokeapi.co/api/v2/pokemon";
-string BASE_TYPE_ADDRESS = "https://pokeapi.co/api/v2/type";
-HttpClient client = new HttpClient();
-HashSet<string> COMMANDS = new HashSet<string>() { "t", "c", "q" };
-
-
-Console.WriteLine("Welcome to the pokemon app!");
-string command = "c";
-
-while (command != "q")
+internal class Program
 {
-    command = getCommand();
-    if (command is null || !COMMANDS.Contains(command))
+    static string BASE_POKEMON_ADDRESS = "https://pokeapi.co/api/v2/pokemon";
+    static string BASE_TYPE_ADDRESS = "https://pokeapi.co/api/v2/type";
+    static HttpClient client = new HttpClient();
+    private static async Task Main(string[] args)
     {
-        Console.WriteLine("Couldn't recognize command! Try again.");
-    }
-    else
-    {
-        Console.WriteLine($"Handling command: {command}");
-        if (command == "c" || command == "t")
+
+        HashSet<string> COMMANDS = new HashSet<string>() { "t", "c", "q" };
+
+
+        Console.WriteLine("Welcome to the pokemon app!");
+        string command = "c";
+
+        while (command != "q")
         {
-            Console.Write("Enter pokemon name: ");
-            string? pokemonName = Console.ReadLine();
-            if (pokemonName is null)
+            command = getCommand();
+            if (command is null || !COMMANDS.Contains(command))
             {
-                Console.WriteLine("Pokemon name was null. Try again!");
-                break;
-            }
-            HttpResponseMessage? pokemonResponse;
-            try
-            {
-                pokemonResponse = await getPokemonResponse(pokemonName);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Exception occured while getting pokemon {pokemonName}");
-                Console.Write(e.StackTrace);
-                continue;
-            }
-            if (!pokemonResponse.IsSuccessStatusCode)
-            {
-                Console.WriteLine($"Could not find pokemon with name: {pokemonName}. Try again!");
-                continue;
+                Console.WriteLine("Couldn't recognize command! Try again.");
             }
             else
             {
-
-                try
+                Console.WriteLine($"Handling command: {command}");
+                if (command == "c" || command == "t")
                 {
-
-                    Creature? pokemon = await pokemonResponse.Content.ReadFromJsonAsync<Creature>();
-                    if (pokemon is null)
+                    Console.Write("Enter pokemon name: ");
+                    string? pokemonName = Console.ReadLine();
+                    if (pokemonName is null)
                     {
-                        Console.WriteLine("Could not derive Creature object from JSON response below:");
-                        Console.Write(pokemonResponse.Content);
-                        continue;
-
+                        Console.WriteLine("Pokemon name was null. Try again!");
+                        break;
                     }
-                    Console.WriteLine($"{pokemon.Name}, I choose you!");
-                    if (command == "t")
+                    HttpResponseMessage? pokemonResponse;
+                    try
                     {
-                        Console.WriteLine($"{pokemon.Name} has type(s) {pokemon.getTypeString()}");
-                        Dictionary<PokemonType.Type, DamageRelations> typeRelations = new Dictionary<PokemonType.Type, DamageRelations>();
-                        foreach (PokemonType pokemonType in pokemon.Types)
+                        pokemonResponse = await getPokemonResponse(pokemonName);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"Exception occured while getting pokemon {pokemonName}");
+                        Console.Write(e.StackTrace);
+                        continue;
+                    }
+                    if (!pokemonResponse.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine($"Could not find pokemon with name: {pokemonName}. Try again!");
+                        continue;
+                    }
+                    else
+                    {
+
+                        try
                         {
 
-                            string typeName = pokemonType.type.Name;
-
-                            HttpResponseMessage? damageResponse;
-                            try
+                            Creature? pokemon = await pokemonResponse.Content.ReadFromJsonAsync<Creature>();
+                            if (pokemon is null)
                             {
-                                damageResponse = await getDamageRelationsResponse(typeName);
-                                if (!damageResponse.IsSuccessStatusCode)
-                                {
-                                    Console.WriteLine($"Could not find damage type: {typeName}. Try again!");
-                                    continue;
-
-                                }
-                                TypeResponse? response = await damageResponse.Content.ReadFromJsonAsync<TypeResponse>();
-                                if (response is null)
-                                {
-                                    Console.WriteLine("Could not derive TypeResponse object from JSON response below:");
-                                    Console.Write(pokemonResponse.Content);
-                                    continue;
-                                }
-
-
-                                typeRelations.Add(pokemonType.type, response.damage_relations);
-                            }
-                            catch (Exception e)
-                            {
-                                Console.WriteLine($"Exception occured while getting pokemon {typeName}");
-                                Console.Write(e.StackTrace);
+                                Console.WriteLine("Could not derive Creature object from JSON response below:");
+                                Console.Write(pokemonResponse.Content);
                                 continue;
+
                             }
+                            Console.WriteLine($"{pokemon.Name}, I choose you!");
+                            if (command == "t")
+                            {
+                                Console.WriteLine($"{pokemon.Name} has type(s) {pokemon.getTypeString()}");
+                                Dictionary<PokemonType.Type, DamageRelations> typeRelations = new Dictionary<PokemonType.Type, DamageRelations>();
+                                foreach (PokemonType pokemonType in pokemon.Types)
+                                {
+
+                                    string typeName = pokemonType.type.Name;
+
+                                    HttpResponseMessage? damageResponse;
+                                    try
+                                    {
+                                        damageResponse = await getDamageRelationsResponse(typeName);
+                                        if (!damageResponse.IsSuccessStatusCode)
+                                        {
+                                            Console.WriteLine($"Could not find damage type: {typeName}. Try again!");
+                                            continue;
+
+                                        }
+                                        TypeResponse? response = await damageResponse.Content.ReadFromJsonAsync<TypeResponse>();
+                                        if (response is null)
+                                        {
+                                            Console.WriteLine("Could not derive TypeResponse object from JSON response below:");
+                                            Console.Write(pokemonResponse.Content);
+                                            continue;
+                                        }
+
+
+                                        typeRelations.Add(pokemonType.type, response.damage_relations);
+                                    }
+                                    catch (Exception e)
+                                    {
+                                        Console.WriteLine($"Exception occured while getting pokemon {typeName}");
+                                        Console.Write(e.StackTrace);
+                                        continue;
+                                    }
+
+                                }
+                                pokemon.damageRelationsMap = typeRelations;
+                                pokemon.printTypeSummaries();
+                                pokemon.printDamageRelations();
+                            }
+                        }
+                        catch (OperationCanceledException e)
+                        {
+                            Console.Write(e.StackTrace);
+                            Console.WriteLine("Operation cancelled while trying to derive creature object. Try again!");
+
 
                         }
-                        pokemon.damageRelationsMap = typeRelations;
-                        pokemon.printTypeSummaries();
-                        pokemon.printDamageRelations();
                     }
                 }
-                catch (OperationCanceledException e)
+                else if (command == "q")
                 {
-                    Console.Write(e.StackTrace);
-                    Console.WriteLine("Operation cancelled while trying to derive creature object. Try again!");
-
-
+                    Console.WriteLine("Bye!");
+                    Environment.Exit(0);
                 }
             }
         }
-        else if (command == "q")
-        {
-            Console.WriteLine("Bye!");
-            Environment.Exit(0);
-        }
+
+
+
     }
-}
-
-async Task<HttpResponseMessage> getDamageRelationsResponse(string p_Name)
-{
-    client.DefaultRequestHeaders.Accept.Clear();
-    client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
-    string url = $"{BASE_TYPE_ADDRESS}/{p_Name}/";
-    return await client.GetAsync(url);
-
-
-
-}
-
-async Task<HttpResponseMessage> getPokemonResponse(string p_Name)
-{
-    client.DefaultRequestHeaders.Accept.Clear();
-    client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
-    string url = $"{BASE_POKEMON_ADDRESS}/{p_Name}/";
-    return await client.GetAsync(url);
-}
-
-string getCommand()
-{
-    Console.WriteLine("Choose a command: ");
-    Console.WriteLine("c: check pokemon name");
-    Console.WriteLine("t: check pokemon damage types");
-    Console.WriteLine("q: quit");
-    Console.Write("Choice: ");
-    string? command = Console.ReadLine();
-    if (command is null)
+    static async Task<HttpResponseMessage> getDamageRelationsResponse(string p_Name)
     {
-        return "";
-    }
-    return command;
-}
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+        string url = $"{BASE_TYPE_ADDRESS}/{p_Name}/";
+        return await client.GetAsync(url);
 
+
+
+    }
+
+    static async Task<HttpResponseMessage> getPokemonResponse(string p_Name)
+    {
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+        string url = $"{BASE_POKEMON_ADDRESS}/{p_Name}/";
+        return await client.GetAsync(url);
+    }
+
+    static string getCommand()
+    {
+        Console.WriteLine("Choose a command: ");
+        Console.WriteLine("c: check pokemon name");
+        Console.WriteLine("t: check pokemon damage types");
+        Console.WriteLine("q: quit");
+        Console.Write("Choice: ");
+        string? command = Console.ReadLine();
+        if (command is null)
+        {
+            return "";
+        }
+        return command;
+    }
+}
